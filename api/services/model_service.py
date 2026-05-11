ed51cdefd62b175a63d60405ffbf9a64dde2896b
FPC-VIAUC <354412@via.dk>
2026-05-11 13:47:12 +0200

    Add remote storage

--- api/services/model_service.py
+++ api/services/model_service.py
@@ -1,16 +1,30 @@
-import os
-import cloudpickle
-from pathlib import Path
 from sklearn.pipeline import Pipeline
+import cloudpickle
+
+import logging
+from api.config import (
+    AZURE_STORAGE_CONNECTION_STRING,
+    AZURE_BLOB_CONTAINER_NAME,
+    DEFAULT_MODEL,
+)
+from azure.storage.blob import BlobServiceClient, ContainerClient, BlobClient
 
 
 class ModelNotFoundError(BaseException):
     pass
 
 
+logger = logging.getLogger("azure.core.pipeline.policies.http_logging_policy")
+logger.setLevel(logging.WARNING)
+
+blob_service_client: BlobServiceClient = BlobServiceClient.from_connection_string(
+    AZURE_STORAGE_CONNECTION_STRING
+)
+container_client: ContainerClient = blob_service_client.get_container_client(
+    container=AZURE_BLOB_CONTAINER_NAME
+)
+
 _models_cache: dict[str, Pipeline] = {}
-MODELS_PATH: str = str(Path(__file__).resolve().parent.joinpath("./models"))
-DEFAULT_MODEL: str = "DMI"
 
 
 def _get_model_info(model: str | None) -> tuple[str, int | None, int | None]:
@@ -29,12 +43,27 @@ def _get_model_info(model: str | None) -> tuple[str, int | None, int | None]:
     return tuple(model_info)
 
 
-def get_model(model: str | None) -> tuple[Pipeline, str]:
-    models = [
+def get_available_models() -> list[str]:
+    return [
         _get_model_info(f.removesuffix(".pkl"))
-        for f in os.listdir(MODELS_PATH)
-        if Path(MODELS_PATH, f).is_file()
+        for f in container_client.list_blob_names()
     ]
+
+
+def get_model(model: str) -> Pipeline:
+    if model not in _models_cache:
+        blob_client: BlobClient = blob_service_client.get_blob_client(
+            container=AZURE_BLOB_CONTAINER_NAME, blob=f"{model}.pkl"
+        )
+
+        data = blob_client.download_blob().readall()
+        _models_cache[model] = cloudpickle.loads(data)
+
+    return _models_cache[model]
+
+
+def find_model(model: str | None) -> tuple[Pipeline, str]:
+    models = get_available_models()
 
     model_info = _get_model_info(model)
     for i, info in enumerate(model_info):
@@ -50,8 +79,4 @@ def get_model(model: str | None) -> tuple[Pipeline, str]:
 
     name, major_version, minor_version = models[0]
     final_name = f"{name}-{major_version}_{minor_version}"
-    if final_name not in _models_cache:
-        with open(Path(MODELS_PATH, f"{final_name}.pkl"), "rb") as f:
-            _models_cache[final_name] = cloudpickle.load(f)
-
-    return _models_cache[final_name], final_name
+    return get_model(final_name), final_name
